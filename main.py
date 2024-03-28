35a97e827274a24f912c0365847f2041290d1c1a
ppannawitt <106550775+ppannawitt@users.noreply.github.com>
2024-03-28 07:21:44 +0000

    submit

--- main.py
+++ main.py
@@ -4,17 +4,49 @@ import time
 
 # Part 1
 def clock(n):
-    # Your code here
-    pass
+  """
+  clock
+  """
+  prev = ""
+  while (n>0):
+    timenow = datetime.now()
+    timenow = timenow.strftime("%H:%M:%S")
+    if (prev!=timenow):
+      prev = timenow
+      print(timenow, end='')
+      print("\r", end='')
+      n-=1
+
+  
 
 
 # Part 2
 def lcm(a, b):
-    # Your code here
-    pass
+  """
+  lcm
+  """
+  #return a*b//gcd(a,b)
+  original_a = a
+  original_b = b
+  while (a!=b):
+    if a<b:
+      a += original_a
+    else:
+      b += original_b
+
+  return a
+
 
 
 # Part 3
 def gcf(a, b):
-    # Your code here
-    pass
+  """
+  gcf
+  """
+  while a!=0:
+    newa = b%a
+    b = a
+    a = newa
+  return b
+
+clock(3)
